0aff28a5b2c7e323a206d98cdfb58a9e3da0735b
Shiva <shivanangunoori01@gmail.com>
2023-05-13 18:00:00 -0400

    Incremental replay harness and Phase 1 documentation

---
+++ scripts/replay_days.py
@@ -0,0 +1,68 @@
+"""Replay the next N landing days: ingest --once, then incremental dbt build.
+
+Each cycle appends data/benchmark/replay_days.jsonl. Stops if ingest finds
+no next folder. last_batch_date must already sit at the cutoff (run
+ingest --backfill first).
+"""
+
+from __future__ import annotations
+
+import argparse
+import json
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent))
+
+from ingest import ingest
+from paths import BENCHMARK_DIR, LANDING_DIR, REPLAY_LOG_PATH, ensure_data_dirs
+from run_dbt import run_dbt
+
+
+def replay_days(*, landing: Path, days: int) -> list[dict]:
+    ensure_data_dirs()
+    records: list[dict] = []
+    for index in range(1, days + 1):
+        ingested = ingest(landing=landing, mode="once", run_type="incremental")
+        day_batches = [name for name in ingested.get("batches", []) if name != "_reference"]
+        if not day_batches:
+            print(f"Replay stopped after {index - 1} day(s): no next landing folder.")
+            break
+        batch_id = day_batches[0]
+        built = run_dbt("incremental", [])
+        record = {
+            "day_index": index,
+            "batch_id": batch_id,
+            "ingest_rows_written": ingested.get("rows_written", 0),
+            "ingest_duration_seconds": round(float(ingested.get("duration_seconds", 0)), 3),
+            "dbt_duration_seconds": round(float(built.get("duration_seconds", 0)), 3),
+            "dbt_returncode": built.get("returncode", 1),
+        }
+        records.append(record)
+        with REPLAY_LOG_PATH.open("a", encoding="utf-8") as handle:
+            handle.write(json.dumps(record) + "\n")
+        print(
+            f"Replay {index}/{days} {batch_id}: "
+            f"{record['ingest_rows_written']} rows in {record['ingest_duration_seconds']}s, "
+            f"dbt {record['dbt_duration_seconds']}s"
+        )
+    return records
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--landing", type=Path, default=LANDING_DIR)
+    parser.add_argument("--days", type=int, default=30, help="How many landing folders to replay")
+    return parser.parse_args()
+
+
+def main() -> int:
+    args = parse_args()
+    replay_days(landing=args.landing, days=args.days)
+    print(f"Replay log: {REPLAY_LOG_PATH}")
+    print(f"Also see {BENCHMARK_DIR / 'dbt_builds.jsonl'}")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ scripts/run_dbt.py
@@ -0,0 +1,89 @@
+"""Run dbt build and append timing to the Phase 1 run log."""
+
+from __future__ import annotations
+
+import argparse
+import json
+import os
+import subprocess
+import sys
+import time
+import uuid
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parent))
+
+from paths import BENCHMARK_DIR, BRONZE_DIR, PROJECT_ROOT, WAREHOUSE_DIR, ensure_data_dirs
+from run_log import append_run_log
+
+
+def run_dbt(run_type: str, extra_args: list[str]) -> dict:
+    ensure_data_dirs()
+    os.environ["ONELAKE_BRONZE"] = str(BRONZE_DIR).replace("\\", "/")
+    os.environ["ONELAKE_DUCKDB"] = str(WAREHOUSE_DIR / "onelake.duckdb").replace("\\", "/")
+    run_id = str(uuid.uuid4())
+    dbt_exe = PROJECT_ROOT / ".venv" / "Scripts" / "dbt.exe"
+    if not dbt_exe.exists():
+        dbt_exe = Path("dbt")
+
+    command = [
+        str(dbt_exe),
+        "build",
+        "--project-dir",
+        str(PROJECT_ROOT / "transform"),
+        "--profiles-dir",
+        str(PROJECT_ROOT / "transform"),
+        *extra_args,
+    ]
+    started = time.perf_counter()
+    completed = subprocess.run(command, cwd=str(PROJECT_ROOT), check=False)
+    elapsed = time.perf_counter() - started
+
+    append_run_log(
+        run_id=run_id,
+        layer="dbt",
+        model="build",
+        rows_read=0,
+        rows_written=0,
+        duration_seconds=elapsed,
+        run_type=run_type,
+        extra={"returncode": completed.returncode, "command": command},
+    )
+
+    BENCHMARK_DIR.mkdir(parents=True, exist_ok=True)
+    bench_path = BENCHMARK_DIR / "dbt_builds.jsonl"
+    with bench_path.open("a", encoding="utf-8") as handle:
+        handle.write(
+            json.dumps(
+                {
+                    "run_id": run_id,
+                    "run_type": run_type,
+                    "duration_seconds": round(elapsed, 3),
+                    "returncode": completed.returncode,
+                }
+            )
+            + "\n"
+        )
+
+    print(f"dbt build ({run_type}) finished in {elapsed:.1f}s with exit {completed.returncode}")
+    if completed.returncode != 0:
+        raise SystemExit(completed.returncode)
+    return {"run_id": run_id, "duration_seconds": elapsed, "returncode": completed.returncode}
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("--run-type", default="incremental", choices=("full", "incremental"))
+    parser.add_argument("dbt_args", nargs=argparse.REMAINDER)
+    return parser.parse_args()
+
+
+def main() -> int:
+    args = parse_args()
+    extra = [arg for arg in args.dbt_args if arg != "--"]
+    run_dbt(args.run_type, extra)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
